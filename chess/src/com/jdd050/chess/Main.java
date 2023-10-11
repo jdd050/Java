dd5d8b6f0673aab8a8bf61ece71c668b4ba68b5a
jd05 <124862332+jdd050@users.noreply.github.com>
2023-10-11 18:19:43 -0500

    add calculator app

--- chess/src/com/jdd050/chess/Main.java
+++ chess/src/com/jdd050/chess/Main.java
@@ -4,6 +4,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -44,6 +46,26 @@ public class Main extends JPanel implements ActionListener {
 				squares.get(file).add(square);
 			}
 		}
+		
+		createGameGrid();
+		addPieceTextures();
+		// assign actionListeners to each piece
+		for (JLabel piece : pieces) {
+			piece.addMouseMotionListener(new MouseMotionListener() {
+
+				@Override
+				public void mouseDragged(MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+
+				@Override
+				public void mouseMoved(MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}});
+		}
+		
 	}
 	
 	/*
@@ -323,13 +345,6 @@ public class Main extends JPanel implements ActionListener {
 	private static void createAndShowGui() {
 		// calls
 		Main mainPanel = new Main();
-		mainPanel.createGameGrid();
-		mainPanel.addPieceTextures();
-		
-		// assign actionListeners to each piece
-		for (JLabel piece : pieces) {
-			System.out.println(piece.getName());
-		}
 		
 		// make and configure window
 		mainPanel.setBackground(Color.gray);
